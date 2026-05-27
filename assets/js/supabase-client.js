1e2da3d3adb6e4918b1c8e56087cda069659a679
Nexa Pixel <johanessuaktiwa@gmail.com>
2026-05-27 15:13:01 +0700

    Create supabase-client.js

---
+++ assets/js/supabase-client.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://rnquhkrisyqfohbukxle.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_ttMemHjB4n45FU6QVKpw0Q_QSQxTRRh";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
